e2af8b0a0f7e6a3f6c358657b667ead7e5da593c
Zhanylai <87288278+Zhanyl3909@users.noreply.github.com>
2023-05-27 21:31:26 +0900

    Update 217. Contains Duplicate.java

--- 217.Contains_Duplicate/217. Contains Duplicate.java
+++ 217.Contains_Duplicate/217. Contains Duplicate.java
@@ -10,3 +10,22 @@ class Solution {
     }
 }
 
+
+
+
+
+
+//HashMap Approach
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++) {
+            if(map.containsKey(nums[i])) {
+                return true;
+            }
+            map.put(nums[i],1);
+        }
+        return false;
+    }
+}
