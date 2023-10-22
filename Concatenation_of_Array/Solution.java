330bbc8fe9f68dbddbe6694727658a8dc6555341
Zhanylai <87288278+Zhanyl3909@users.noreply.github.com>
2023-10-22 23:51:25 +0900

    Update Solution.java

--- Concatenation_of_Array/Solution.java
+++ Concatenation_of_Array/Solution.java
@@ -3,6 +3,7 @@ class Solution {
         int len = nums.length ;
         int[] arrCat = new int[len*2];
 
+        
         for(int i=0; i<len; i++) {
                 arrCat[i] = nums[i];
                 arrCat[i+len] = nums[i];
